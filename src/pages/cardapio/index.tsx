33ca6f6ca7153adf77863b4916f8f2c231fd9d2a
Mickael L. Dias da Silva <mickaelsilva@sad.infovia-mt>
2022-05-24 13:06:36 -0400

    first commit

---
+++ src/pages/cardapio/index.tsx
@@ -0,0 +1,34 @@
+import styles from "./Cardapio.module.scss";
+import logo from 'assets/logo.png'
+import Buscador from "./buscador";
+import { useState } from "react";
+import Filtros from "./filtros";
+import Ordenador from "./Ordenador";
+import Itens from './itens'
+
+export default function Cardapio() {
+  const [busca, setBusca] = useState('')
+  const [filtro, setFiltro] = useState<number | null>(null)
+  const [ordenador, setOrdenador] = useState('')
+  return (
+    <main>
+      <nav className={styles.menu}>
+        <img src={logo} alt='logo do aluroni'/>
+      </nav>
+      <header className={styles.header}>
+        <div className={styles.header__text}>
+          A casa do codigo e da massa
+        </div>
+      </header>
+      <section className={styles.cardapio}>
+        <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+        <Buscador busca={busca} setBusca={setBusca}/>
+        <div className={styles.cardapio__filtros}>
+          <Filtros filtro={filtro} setFiltro={setFiltro}/>
+          <Ordenador ordenador={ordenador} setOrdenador={setOrdenador}/>
+        </div>
+        <Itens busca={busca} filtro={filtro} ordenador={ordenador}/>
+      </section>
+    </main>
+  )
+}
